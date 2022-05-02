997a3681b504ea09ce7e4911498e120d35ed6bf6
Corey <40738690+C1200@users.noreply.github.com>
2022-05-02 17:56:20 +0100

    fix formatting

--- lib/index.ts
+++ lib/index.ts
@@ -326,7 +326,7 @@ const registerListeners = (
         clientId: clientId.substring(0, 12), // this information is quite sensitive
         transport: socket.conn.transport.name,
       },
-      ...socket.data
+      ...socket.data,
     });
 
     adminNamespace.emit("socket_connected", serialize(socket, nsp.name));
